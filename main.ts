bac86cd5546fbb3ba3a974601deefa8d44cf5c5d
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-10-11 14:47:58 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -13,43 +13,36 @@ pins.digitalWritePin(DigitalPin.P15, 0)
 pins.digitalWritePin(DigitalPin.P14, 0)
 
 input.onButtonPressed(Button.A, function () {
-
   // red
   basic.showString(' Red ')
   pins.digitalWritePin(DigitalPin.P16, 1)
   pins.digitalWritePin(DigitalPin.P15, 0)
   pins.digitalWritePin(DigitalPin.P14, 0)
-
   // green
   basic.showString('Green ')
   pins.digitalWritePin(DigitalPin.P16, 0)
   pins.digitalWritePin(DigitalPin.P15, 1)
   pins.digitalWritePin(DigitalPin.P14, 0)
-
   // blue
   basic.showString('Blue ')
   pins.digitalWritePin(DigitalPin.P16, 0)
   pins.digitalWritePin(DigitalPin.P15, 0)
   pins.digitalWritePin(DigitalPin.P14, 1)
-
   // yellow
   basic.showString('Yellow ')
   pins.digitalWritePin(DigitalPin.P16, 1)
   pins.digitalWritePin(DigitalPin.P15, 1)
   pins.digitalWritePin(DigitalPin.P14, 0)
-
   // magenta
   basic.showString('Magenta ')
   pins.digitalWritePin(DigitalPin.P16, 1)
   pins.digitalWritePin(DigitalPin.P15, 0)
   pins.digitalWritePin(DigitalPin.P14, 1)
-
   // cyan
   basic.showString('Cyan ')
   pins.digitalWritePin(DigitalPin.P16, 0)
   pins.digitalWritePin(DigitalPin.P15, 1)
   pins.digitalWritePin(DigitalPin.P14, 1)
-
   // white
   basic.showString('White')
   pins.digitalWritePin(DigitalPin.P16, 1)
